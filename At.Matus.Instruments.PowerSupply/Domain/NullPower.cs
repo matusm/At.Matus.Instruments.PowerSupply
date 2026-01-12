ef00deea5b3aa68070dffb0d10b32a5aedff14b8
Michael Matus <michael@matus.at>
2026-01-12 22:16:51 +0100

    Add NullPower: a null object implementation of IPowerSupply
    Introduced the NullPower class in the Domain namespace, providing a do-nothing implementation of the IPowerSupply interface. All methods return default values or perform no actions. Updated the project file to include NullPower.cs in the build.

---
+++ At.Matus.Instruments.PowerSupply/Domain/NullPower.cs
@@ -0,0 +1,21 @@
+﻿using At.Matus.Instruments.PowerSupply.Abstractions;
+
+namespace At.Matus.Instruments.PowerSupply.Domain
+{
+    public class NullPower : IPowerSupply
+    {
+        public string InstrumentManufacturer => string.Empty;
+        public string InstrumentType => string.Empty;
+        public string InstrumentFirmwareVersion => string.Empty;
+        public string InstrumentSerialNumber => string.Empty;
+
+        public void TurnOn() { }
+        public void TurnOff() { }
+        public void SetVoltage(double voltage) { }
+        public void SetCurrent(double current) { }
+        public double GetVoltage() => 0;
+        public double GetCurrent() => 0;
+        public double GetSetVoltage() => 0;
+        public double GetSetCurrent() => 0;
+    }
+}
